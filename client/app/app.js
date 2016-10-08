e4a2d65ffc0de0cc190d672ab05f0aa65a1aa5f9
Vangogh500 <kmatsuda@umass.edu>
2016-10-08 04:16:27 -0400

    added route for testing purposes

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route} from 'react-router'
+
 import GameContainer from './gamecontainer'
 import Navbar from './navbar'
 import {main} from './game'
@@ -15,10 +17,24 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-);
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>TEST</h1>
+        <canvas id="canvas"></canvas>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App} />
+    <Route path="/test" component={Test} />
+  </Router>
+), document.getElementById('app'));
 
 console.log(main)
 main()
