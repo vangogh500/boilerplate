247c201de57349fe6f588638a2aead651b7cf8c0
Vangogh500 <kmatsuda@umass.edu>
2016-10-07 23:54:26 -0400

    boiler plat for app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+class App extends React.Component {
+  render() {
+    return (
+      <h1>HELLO</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
