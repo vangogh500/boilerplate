6e3b7a295fc0c71b66829ddfbfa0dfc5a3a68813
arowshaft <ruoting@umass.edu>
2016-10-08 17:36:36 -0400

    edge remake

---
+++ client/app/structures/edge.js
@@ -0,0 +1,24 @@
+export default class Edge {
+  constructor(vertexA, vertexB){
+    this.vertexA = vertexA;
+    this.vertexB = vertexB;
+  }
+
+  get getVertexA(){
+    return this.vertexA;
+  }
+
+  get getVertexB(){
+    return this.vertexB;
+  }
+
+
+  get upstream()
+  {
+
+  }
+  get downstream()
+  {
+
+  }
+}
