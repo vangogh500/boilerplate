d16604b5731a1c4d1ff4312ece03a7c57f8ffcad
Jacob Goldman <jacobgoldman@1x-vl925-72-19-104-205.wireless.umass.edu>
2016-10-08 20:46:34 -0400

    added back getOtherVertex()

--- client/app/structures/edge.js
+++ client/app/structures/edge.js
@@ -15,6 +15,20 @@ export default class Edge {
   get getVertexB(){
     return this.vertexB;
   }
+
+  // returns null if vertex is not contained, otherwise returns other vertex
+  getOtherVertex(vertex) {
+    if (this.vertexA == vertex){
+      return this.vertexB
+    }
+    else if(this.vertexB == vertex) {
+      return this.vertexA
+    }
+    else {
+      return null
+    }
+  }
+
   setAntiFlow(antiFlow)
   {
     this.antiFlow = antiFlow;
